e67f2ec3037ed1d4526cfca3e9c039a4918daf82
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-02-15 21:04:03 +0100

    Create getters and modify state in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,33 @@
 import { createStore } from 'vuex'
+import puzzles from "../assets/puzzles.json";
 
 export default createStore({
   state: {
-    currentPuzzleId: 0,
     userScore: 200,
+    puzzlesQuantity: 30,
+    currentPuzzleId: 0,
+    puzzles: puzzles
+
   },
   getters: {
     userScore(state) {
       return state.userScore;
     },
+    puzzlesQuantity(state) {
+      return state.puzzlesQuantity;
+    },
+
     currentPuzzleId(state) {
       return state.currentPuzzleId;
     },
+    currentPuzzleImage(state) {
+      return state.puzzles[state.currentPuzzleId].image;
+    },
+    currentPuzzleSentence(state) {
+      return state.puzzles[state.currentPuzzleId].sentence;
+    },
   },
+
   mutations: {
     increment: state => state.currentPuzzleId++,
   },
