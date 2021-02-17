47aff05582a2b99ab47bd8f88843e37a208bbb4d
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-02-17 20:37:34 +0100

    Change structure of sentence mask and css

--- src/components/PuzzleSentenceBox.vue
+++ src/components/PuzzleSentenceBox.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="puzzle-sentence-box">
-    <div class="puzzle-gaps">
-      <span class="sentence-mask">{{ sentenceMask }}</span>
+    <div class="sentence-mask">
+      <div v-for="(letter, index) in sentenceMask" :key="index">
+        <span class="sentence-mask__single-letter" :index="index">
+          {{ letter }}
+        </span>
+      </div>
     </div>
 
-    <div class="letters-box">
+    <div class="letters">
       <div v-for="(letter, index) in sentenceScratteredLetters" :key="index">
-        <span class="letters-box__single-letter" :index="index">
+        <span class="letters__single-letter" :index="index">
           {{ letter }}
         </span>
       </div>
@@ -19,8 +23,7 @@ export default {
 
   data() {
     return {
-      sentenceMask: "",
-      sentenceLetters: [],
+      sentenceMask: [],
       sentenceScratteredLetters: [],
     };
   },
@@ -29,7 +32,9 @@ export default {
       String.prototype.cleanup = function () {
         return this.toLowerCase().replace(/[a-zA-Z]/g, "_");
       };
-      this.sentenceMask = this.$store.getters.currentPuzzleSentence.cleanup();
+      this.sentenceMask = this.$store.getters.currentPuzzleSentence
+        .cleanup()
+        .split("");
     },
 
     convertSentenceAsSplitedArray(sentence) {
@@ -43,7 +48,10 @@ export default {
       return (string.slice(0, index) + string.slice(index + 1)).split("");
     },
 
-    scatterLetters(splitedSentence) {
+    scatterLetters() {
+      var splitedSentence = this.convertSentenceAsSplitedArray(
+        this.$store.getters.currentPuzzleSentence
+      );
       var sentenceLength = splitedSentence.length;
       var sentenceLengthTemp = sentenceLength;
 
@@ -62,8 +70,7 @@ export default {
   },
   mounted() {
     this.createSentenceMask();
-    this.sentenceLetters = this.convertSentenceAsSplitedArray(this.$store.getters.currentPuzzleSentence);
-    this.scatterLetters(this.sentenceLetters);
+    this.scatterLetters();
   },
 };
 </script>
@@ -74,13 +81,14 @@ export default {
   font-size: 50px;
   letter-spacing: 5px;
 }
-.letters-box {
+.letters,
+.sentence-mask {
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 50px;
 }
-.letters-box__single-letter {
+.letters__single-letter {
   border: 1px solid black;
   width: 40px;
   height: 40px;
@@ -90,7 +98,14 @@ export default {
   line-height: 40px;
   text-transform: capitalize;
 }
-.letters-box__single-letter:hover {
+.letters__single-letter:hover {
   cursor: pointer;
+}
+
+.sentence-mask__single-letter {
+  display: inline-block;
+  font-size: 50px;
+  width: 30px;
+  margin: 0 5px;
 }
 </style>
