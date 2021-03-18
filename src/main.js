e9f344a6963f8016b08ef7b03fdd31b3f3d0807a
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-18 14:48:03 +0100

    Install axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = 'https://127.0.0.1:8000/api/';
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
