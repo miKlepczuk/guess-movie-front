336e5fbcfdd70371790fdd7878c953563d7c11f0
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-24 10:11:45 +0100

    Move all puzzles handling from to separated module in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,81 +1,30 @@
 import { createStore } from 'vuex'
-import axios from 'axios'
 import createPersistedState from "vuex-persistedstate";
 import userModule from "./modules/user";
-import constants from './modules/consts.js'
+import puzzlesModule from "./modules/puzzles";
 
 export default createStore({
   state: {
-    puzzles: [],
     // mask: [{position: 0, letter: 'g', isHinted: false, indexInScrattered: 0}],
     // scratteredLetters: [{position: 0, letter: 'g', isVisible: false}]
   },
 
   getters: {
-    puzzlesQuantity(state) {
-      return state.puzzles.length;
-    },
-
-    isThisTheLastSentence(state, getters) {
-      if (getters.userPuzzleId == getters.puzzlesQuantity) {
-        return true;
-      }
-      return false;
-    },
-    isAllowedToLoadNextSentence(state, getters) {
-      if (getters.userPuzzleId > getters.puzzlesQuantity) {
-        return false;
-      }
-      return true;
-    },
-    isHintAllowed(state, getters) {
-      if (getters.userScore >= constants.POINTS_FOR_SINGLE_HINT)
-        return true;
-      else
-        return false;
-    },
-    currentPuzzleImage(state, getters) {
-      let puzzle = state.puzzles.find(element => element.id == getters.userPuzzleId)
-      if (puzzle)
-        return puzzle.image;
-    },
-
-    currentPuzzleSentence(state, getters) {
-      let puzzle = state.puzzles.find(element => element.id == getters.userPuzzleId)
-      if (puzzle)
-        return puzzle.sentence;
-    },
 
   },
 
   mutations: {
-    setPuzzles(state, puzzles) {
-      state.puzzles = puzzles;
-    },
 
-    clearPuzzlesState(state) {
-      state.puzzles = [];
-    },
   },
+
   actions: {
-    async getPuzzles({ commit }) {
-      let response = await axios.get("puzzles", {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      })
-      commit('setPuzzles', response.data)
-    },
-
-    incrementCurrentPuzzle({ commit, getters }) {
-      if (getters.isThisTheLastSentence == false) {
-        commit('incrementUserPuzzleId');
-      }
-    },
 
   },
+
   modules: {
-    user: userModule
+    user: userModule,
+    puzzles: puzzlesModule
   },
+
   plugins: [createPersistedState()],
 })
---
+++ src/store/modules/puzzles.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import constants from './consts.js'
+
+
+export default {
+    state: {
+        puzzles: [],
+    },
+
+    getters: {
+        puzzlesQuantity(state) {
+            return state.puzzles.length;
+        },
+
+        isThisTheLastSentence(state, getters) {
+            if (getters.userPuzzleId == getters.puzzlesQuantity) {
+                return true;
+            }
+            return false;
+        },
+        isAllowedToLoadNextSentence(state, getters) {
+            if (getters.userPuzzleId > getters.puzzlesQuantity) {
+                return false;
+            }
+            return true;
+        },
+        isHintAllowed(state, getters) {
+            if (getters.userScore >= constants.POINTS_FOR_SINGLE_HINT)
+                return true;
+            else
+                return false;
+        },
+        currentPuzzleImage(state, getters) {
+            let puzzle = state.puzzles.find(element => element.id == getters.userPuzzleId)
+            if (puzzle)
+                return puzzle.image;
+        },
+
+        currentPuzzleSentence(state, getters) {
+            let puzzle = state.puzzles.find(element => element.id == getters.userPuzzleId)
+            if (puzzle)
+                return puzzle.sentence;
+        },
+    },
+
+    mutations: {
+        setPuzzles(state, puzzles) {
+            state.puzzles = puzzles;
+        },
+
+        clearPuzzlesState(state) {
+            state.puzzles = [];
+        },
+    },
+    actions: {
+        async getPuzzles({ commit }) {
+            let response = await axios.get("puzzles", {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token"),
+                },
+            })
+            commit('setPuzzles', response.data)
+        },
+
+        incrementCurrentPuzzle({ commit, getters }) {
+            if (getters.isThisTheLastSentence == false) {
+                commit('incrementUserPuzzleId');
+            }
+        },
+    },
+
+}
