763f4708c0f3f58f7bdeb80c0fda350b786e3f0f
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-24 10:01:29 +0100

    Remove axios config from main.js

--- src/main.js
+++ src/main.js
@@ -11,15 +11,8 @@ const API_URL = 'https://127.0.0.1:8000/api/';
 
 axios.defaults.baseURL = API_URL;
 
-const token = localStorage.getItem('token')
-if (token) {
-    axios.defaults.headers.common['Authorization'] = "Bearer " + token
-}
-
 const app = createApp(App)
 app.use(store)
 app.use(router)
 app.use(VueAxios, axios)
 app.mount('#app')
-
-app.config.globalProperties.API_URL = API_URL
