b9ec1d687600e9249d98cf875a7538b947c0bb75
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-28 21:11:22 +0200

    Create presentation box on homepage

---
+++ src/components/Presentation.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="presentation">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <h1 class="title">Guess what</h1>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
+            blandit libero, non egestas risus. Nam interdum mattis augue, non
+            sodales nisl.
+          </p>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <img src="../assets/images/home.jpg" />
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <router-link
+            :to="{ name: 'login' }"
+            class="btn btn-primary btn-lg btn-play"
+          >
+            Let's play the game!
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Presentation",
+
+  methods: {},
+  created() {},
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style scoped>
+.title {
+  font-family: "Londrina Solid", cursive;
+  font-size: 50px;
+  margin-bottom: 30px;
+}
+
+.btn-play {
+  margin-bottom: 15px;
+}
+
+@media (max-width: 768px) {
+  .title {
+    font-size: 40px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="home">
     <GameBoard v-if="puzzlesQuantity > 0" />
-    <div v-else>You must be logged in.</div>
+    <Presentation v-if="showPresentation == true && puzzlesQuantity == 0" />
   </div>
 </template>
 
 <script>
 import GameBoard from "@/components/GameBoard.vue";
+import Presentation from "@/components/Presentation.vue";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Home",
   components: {
     GameBoard,
+    Presentation,
   },
-
+  data() {
+    return {
+      showPresentation: false,
+    };
+  },
   created() {
+    this.showPresentation = !this.isAuthorized;
     if (this.isAuthorized == true) {
-        this.getPuzzles();
+      this.getPuzzles();
     }
   },
   computed: {
