daf8a73470545ad01a958986e0b08de116b9969f
Milena Klepczuk <milena.klepczuk@gmail.com>
2021-03-30 21:58:06 +0200

    Add Alert Component and validation for Reset Password form

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -7,48 +7,45 @@
         <div class="col">
           <div class="d-flex flex-row justify-content-center">
             <div class="login-form">
-              <form @submit.prevent="submit">
+              <Form @submit="submit" :validation-schema="schema">
                 <div class="avatar">
                   <img src="../assets/images/avatar.svg" />
                 </div>
                 <h4 class="modal-title">Reset Your Password</h4>
                 <p>Create a new password for your account.</p>
 
-                <div
-                  class="alert alert-success"
-                  v-if="successMessage.length > 0"
-                >
-                  {{ successMessage }}
-                </div>
-                <div class="alert alert-danger" v-if="errorMessage.length > 0">
-                  {{ errorMessage }}
+                <Alert :isError="isError" :message="message" />
+
+                <div class="form-group">
+                  <Field
+                    name="password"
+                    class="form-control"
+                    placeholder="Password"
+                    v-model="form.password"
+                    type="password"
+                  />
+                  <ErrorMessage class="field__error-message" name="password" />
                 </div>
 
                 <div class="form-group">
-                  <input
-                    type="password"
-                    class="form-control"
-                    placeholder="Password"
-                    required="required"
-                    v-model="form.password"
-                  />
-                </div>
-                <div class="form-group">
-                  <input
-                    type="password"
+                  <Field
+                    name="confirmPassword"
                     class="form-control"
                     placeholder="Confirm password"
-                    required="required"
                     v-model="form.confirmPassword"
+                    type="password"
+                  />
+                  <ErrorMessage
+                    class="field__error-message"
+                    name="confirmPassword"
                   />
                 </div>
                 <input
                   type="submit"
                   class="btn btn-primary btn-block btn-lg"
                   value="Reset password"
-                  :disabled="errorMessage.length > 0 && isSubmited == false"
                 />
-              </form>
+              </Form>
             </div>
           </div>
         </div>
@@ -60,64 +57,61 @@
 <script>
 import { mapActions } from "vuex";
 import ContentLoader from "@/components/ContentLoader.vue";
+import Alert from "@/components/Alert.vue";
+import { Field, Form, ErrorMessage } from "vee-validate";
+import * as Yup from "yup";
 
 export default {
   name: "ResetPassword",
   components: {
     ContentLoader,
+    Alert,
+    Field,
+    Form,
+    ErrorMessage,
   },
   data() {
+    const schema = Yup.object({
+      password: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .required("Password is required"),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Passwords must match")
+        .required("Confirm Password is required"),
+    });
     return {
       form: {
         password: "",
         confirmPassword: "",
         recoveryKey: "",
       },
-      errorMessage: "",
-      successMessage: "",
-      isSubmited: false,
+      message: "",
+      isError: false,
       isRequestProcessing: false,
+      schema,
     };
   },
 
   methods: {
     ...mapActions(["resetPassword"]),
     async submit() {
-      this.form.recoveryKey = this.$route.query.recovery_key;
-      this.errorMessage = "";
-      this.successMessage = "";
-      this.isSubmited = false;
-
-      this.isValidatePasswords();
-      if (this.errorMessage.length == 0) {
-        this.isRequestProcessing = true;
-
-        try {
-          await this.resetPassword(this.form);
-          this.successMessage = "Your password has been changed";
-
-          new Promise((resolve) => {
-            setTimeout(() => {
-              this.$router.push({ name: "login" });
-              console.log("2 seconds up, resolving myTimerPromise");
-              resolve();
-            }, 3000);
-          });
-        } catch (error) {
-          this.isSubmited = true;
-          this.errorMessage = error.response.data.message;
-        }
-        this.isRequestProcessing = false;
+      this.isRequestProcessing = true;
+      try {
+        this.form.recoveryKey = this.$route.query.recovery_key;
+        await this.resetPassword(this.form);
+        this.isError = false;
+        this.message = "Your password has been changed";
+        new Promise((resolve) => {
+          setTimeout(() => {
+            this.$router.push({ name: "login" });
+            resolve();
+          }, 3000);
+        });
+      } catch (error) {
+        this.isError = true;
+        this.message = error.response.data.message;
       }
-    },
-    isValidatePasswords() {
-      if (this.form.password != this.form.confirmPassword) {
-        this.errorMessage = "The password confirmation does not match";
-      } else if (this.form.password.length < 6) {
-        this.errorMessage = "Your password must be at least 6 characters long";
-      } else {
-        this.errorMessage = "";
-      }
+      this.isRequestProcessing = false;
     },
   },
 };
